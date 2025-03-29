b8f81b332490603e3469c8aa9e8e7a69bda7ae2a
Angelo Tinti <angelo.tinti@studio.unibo.it>
2025-03-29 16:19:05 +0100

    Add Power model

---
+++ src/main/scala/model/Power.scala
@@ -0,0 +1,9 @@
+package model
+
+abstract class Power(val name: String)
+
+object Power:
+  case class SeeYourCard() extends Power("See one of your cards")
+  case class SeeYourOpponentCard() extends Power("See your opponent card")
+  case class ChangeOneOfYourCardWithOpponent() extends Power("Change one of your card with opponent")
+  
