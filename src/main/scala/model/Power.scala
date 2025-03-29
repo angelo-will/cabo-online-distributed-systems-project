3acb95d50079f8d07a54647a5092c08ed2c9bbc8
Angelo Tinti <angelo.tinti@studio.unibo.it>
2025-03-29 16:20:14 +0100

    Add no power in powers possibilities

--- src/main/scala/model/Power.scala
+++ src/main/scala/model/Power.scala
@@ -3,6 +3,7 @@ package model
 abstract class Power(val name: String)
 
 object Power:
+  case class NoPower() extends Power("This card has no power")
   case class SeeYourCard() extends Power("See one of your cards")
   case class SeeYourOpponentCard() extends Power("See your opponent card")
   case class ChangeOneOfYourCardWithOpponent() extends Power("Change one of your card with opponent")
