059b49d73f282d8aefc873de10eba72385157acc
Michele Bachetti <michele.bachetti@studio.unibo.it>
2025-05-05 16:51:04 +0200

    update: tests update to work with actors inside a cluster (necessary for testing the distributed data)

--- cabo-server/src/test/scala/ServerTest.scala
+++ cabo-server/src/test/scala/ServerTest.scala
@@ -1,15 +1,14 @@
-import akka.actor.testkit.typed.scaladsl.{ActorTestKit, ScalaTestWithActorTestKit, TestProbe}
+import akka.actor.testkit.typed.scaladsl.{ScalaTestWithActorTestKit, TestProbe}
 import akka.actor.typed.ActorRef
+import akka.cluster.typed.{Cluster, Join}
 import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
 import org.scalatest.wordspec.AnyWordSpecLike
 import org.scalatest.matchers.should.Matchers
 
 import scala.concurrent.duration.*
-
 import model.GameInConstruction
 import model.GameParameters
-import utils.ServerMessages
-import utils.Message
+import utils.{Message, ServerMessages}
 
 class ServerTest extends ScalaTestWithActorTestKit
   with AnyWordSpecLike
@@ -23,9 +22,16 @@ class ServerTest extends ScalaTestWithActorTestKit
   var server: ActorRef[Message] = _
   var testProbe: TestProbe[Message] = _
 
+  override def beforeAll(): Unit =
+    val cluster = Cluster.get(testKit.system)
+    cluster.manager.tell(Join.create(cluster.selfMember.address))
+
   override def beforeEach(): Unit =
     server = testKit.spawn(Server(serverCode))
     testProbe = testKit.createTestProbe[Message]()
+
+  override def afterAll(): Unit =
+    testKit.shutdownTestKit()
 
   "Server" must {
     "send empty games list" when {
@@ -34,11 +40,14 @@ class ServerTest extends ScalaTestWithActorTestKit
         testProbe.expectMessage(ServerMessages.GamesList(Seq()))
       }
     }
-    "send a GameRegistered messagge" when {
+    "send a GameRegistered message" when {
       "someone sends to Server a RegisterGame message" in {
         val game = GameInConstruction("codeGame", GameParameters(maxTimeRound = 10), List.empty)
         server ! ServerMessages.RegisterGame(game, testProbe.ref)
         testProbe.expectMessage(ServerMessages.GameRegistered(game, server))
+
+        //Remove the game from the sever because the list is persistent between test
+        server ! ServerMessages.StartGame(game, testProbe.ref)
       }
     }
     "send a GamesList message with games not started" when {
@@ -52,6 +61,10 @@ class ServerTest extends ScalaTestWithActorTestKit
         val message = testProbe.receiveMessages(3, 5.seconds).filter(_.isInstanceOf[ServerMessages.GamesList]).head
 
         message mustBe ServerMessages.GamesList(List(game1, game2))
+
+        //Remove the games from the sever because the list is persistent between test
+        server ! ServerMessages.StartGame(game1, testProbe.ref)
+        server ! ServerMessages.StartGame(game2, testProbe.ref)
       }
     }
     "send a GameList message without a game" when {
