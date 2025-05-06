ee62c31da5a96d9ec5a63691d3f09505feb51bb1
Michele Bachetti <michele.bachetti@studio.unibo.it>
2025-05-06 18:14:15 +0200

    update: add scaladoc and new message for updating a game

--- cabo-commons/src/main/scala/utils/ServerMessages.scala
+++ cabo-commons/src/main/scala/utils/ServerMessages.scala
@@ -7,15 +7,57 @@ object ServerMessages:
   trait ServerCommand extends Message
   
   type Games = Seq[GameInConstruction]
-  
+
+  /**
+   * Message used to register a game to the server
+   *
+   * @param game - game to register
+   * @param replyTo - ActorRef of the client that send the message
+   */
   case class RegisterGame(game: GameInConstruction, replyTo: ActorRef[Message]) extends ServerCommand
-  
+
+  /**
+   * Reply message for [[RegisterGame]] to acknowledge the client that his game has been registered
+   *
+   * @param game - the game that has been registered
+   * @param replyTo - the ActorRef of the server that served the request
+   */
   case class GameRegistered(game: GameInConstruction, replyTo: ActorRef[Message]) extends ServerCommand
-  
+
+  /**
+   * Message to indicate that the game has started so it has to be removed from the list
+   *
+   * @param game - game that has started
+   * @param replyTo - ActorRef of the client that started the game
+   */
   case class StartGame(game: GameInConstruction, replyTo: ActorRef[Message]) extends ServerCommand
-  
+
+  /**
+   * Message to indicate that the game has been aborted so it has to be removed from the list
+   *
+   * @param game - game that has started
+   * @param replyTo - ActorRef of the client that started the game
+   */
   case class AbortGame(game: GameInConstruction, replyTo: ActorRef[Message]) extends ServerCommand
-  
+
+  /**
+   * Message used by a client to request the list of joinable games
+   *
+   * @param replyTo - ActorRef of the client that requested the list
+   */
   case class GetGames(replyTo: ActorRef[Message]) extends ServerCommand
-  
+
+  /**
+   * Reply message for [[GetGames]]
+   *
+   * @param games - the list of games
+   */
   case class GamesList(games: Games) extends ServerCommand
+
+  /**
+   * Update the information about an already registered game
+   *
+   * @param game - game that has to updated, contains the new information
+   * @param replyTo - ActorRef of the client that requested the update
+   */
+  case class UpdateGame(game: GameInConstruction, replyTo: ActorRef[Message]) extends ServerCommand
