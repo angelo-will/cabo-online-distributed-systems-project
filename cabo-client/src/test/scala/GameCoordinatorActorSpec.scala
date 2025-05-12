f4ffa698e8db67024ffbe03831cbc41e1318e473
Angelo Tinti <angelo.tinti@studio.unibo.it>
2025-05-12 17:47:16 +0200

    Refactor code of GameCoordinatorActorSpec.scala

--- cabo-client/src/test/scala/GameCoordinatorActorSpec.scala
+++ cabo-client/src/test/scala/GameCoordinatorActorSpec.scala
@@ -2,11 +2,10 @@ import akka.actor.testkit.typed.scaladsl.{ScalaTestWithActorTestKit, TestProbe}
 import akka.actor.typed.ActorRef
 import controller.GameCoordinatorActor
 import model.Suit.*
-import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
-import org.scalatest.wordspec.AnyWordSpecLike
-import org.scalatest.matchers.should.Matchers
-
 import model.{Card, Game, Power}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
 import utils.GameCoordinatorMessage.{CardDrawn, DiscardYourNthCard, DrawCardFromDeck, NewTopCardDiscardStack}
 import utils.{GameCoordinatorMessage, Message}
 
@@ -23,6 +22,8 @@ class GameCoordinatorActorSpec extends ScalaTestWithActorTestKit
   // view expects messages from player actor
   // other players' actors expect messages from player actor in the end of the turn to know what happened
 
+  // TODO: TEST WORK ONLY WITH SORTED DECK TO GARANTEE THE REPRODUCIBILTY OF IT
+
   private var gameCoordinatorActor: ActorRef[Message] = _
   private var gameCoordinatorProbe: TestProbe[Message] = _
 
@@ -33,69 +34,11 @@ class GameCoordinatorActorSpec extends ScalaTestWithActorTestKit
     gameCoordinatorActor ! GameCoordinatorMessage.StartGame()
   }
 
-  private def skipFirstShowPhase(): Unit =
-    showYourNthCard(0)
-    showYourNthCard(0)
-    val _ = gameCoordinatorProbe.receiveMessages(2)
-
-  private def jumpARound(): Unit =
-    drawCardFromDeck()
-    val card = gameCoordinatorProbe.expectMessageType[CardDrawn].card
-    card.power match
-      case Power.SeeYourCard() =>
-        showYourNthCard(0)
-        discardCardDrawn()
-        val _ = gameCoordinatorProbe.receiveMessages(2)
-      case Power.SeeYourOpponentCard() =>
-        showAdversaryNthCard(0, 0)
-        discardCardDrawn()
-        val _ = gameCoordinatorProbe.receiveMessages(2)
-      case Power.ChangeOneOfYourCardWithOpponent() =>
-        replaceOwnNthCardWithAdversaryNthOne(0, 0, 0)
-      case Power.NoPower() =>
-        discardCardDrawn()
-        val _ = gameCoordinatorProbe.expectMessageType[NewTopCardDiscardStack]
-    endTurn()
-    val gameInformation = gameCoordinatorProbe.expectMessageType[GameCoordinatorMessage.GameInformation].game
-    newTurn(gameInformation)
-
-  private def skipRoundsUntilDrawThisPower(power: Power): (Card, Game.GameInProgress) =
-    sendGameStatus()
-    var game = gameCoordinatorProbe.expectMessageType[GameCoordinatorMessage.GameInformation].game
-    while game.deckStack.drawFirstCard._1.power != power do
-      jumpARound()
-      sendGameStatus()
-      game = gameCoordinatorProbe.expectMessageType[GameCoordinatorMessage.GameInformation].game
-    drawCardFromDeck()
-    val cardDrawn = gameCoordinatorProbe.expectMessageType[CardDrawn].card
-    (cardDrawn,game)
-
-  private def drawCardFromDeck(): Unit = gameCoordinatorActor ! GameCoordinatorMessage.DrawCardFromDeck()
-
-  private def drawCardFromDiscardStack(): Unit = gameCoordinatorActor ! GameCoordinatorMessage.DrawCardFromDiscardStack()
-
-  private def discardCardDrawn(): Unit = gameCoordinatorActor ! GameCoordinatorMessage.DiscardCardDrawn()
-
-  private def discardNthCard(index: Int): Unit = gameCoordinatorActor ! GameCoordinatorMessage.DiscardYourNthCard(index)
-
-  private def showYourNthCard(i: Int): Unit = gameCoordinatorActor ! GameCoordinatorMessage.ShowYourNthCard(i)
-
-  private def showAdversaryNthCard(playerIndex: Int, cardIndex: Int): Unit = gameCoordinatorActor ! GameCoordinatorMessage.ShowAdversaryNthCard(playerIndex, cardIndex)
-
-  private def replaceOwnNthCardWithAdversaryNthOne(ownCardIndex: Int, adversaryIndex: Int, adversaryCardIndex: Int): Unit =
-    gameCoordinatorActor ! GameCoordinatorMessage.ReplaceOwnNthCardWithAdversaryNthOne(ownCardIndex, adversaryIndex, adversaryCardIndex)
-
-  private def sendGameStatus(): Unit = gameCoordinatorActor ! GameCoordinatorMessage.SendGameStatus(gameCoordinatorProbe.ref)
-
-  private def endTurn(): Unit = gameCoordinatorActor ! GameCoordinatorMessage.EndTurn()
-
-  private def newTurn(game: Game.GameInProgress): Unit = gameCoordinatorActor ! GameCoordinatorMessage.NewTurn(game)
-
   "Single Actor Player" must {
 
     "send status of the game" when {
       "receive the command send status" in {
-        sendGameStatus()
+        sendGameInformation()
         gameCoordinatorProbe.expectMessageType[GameCoordinatorMessage.GameInformation]
       }
       "receive the command to end turn" in {
@@ -117,7 +60,7 @@ class GameCoordinatorActorSpec extends ScalaTestWithActorTestKit
     "send own card value" when {
       "at the start of the game when selected one of own card" in {
         println("START TEST: send own card value when at the start of the game when selected one of own card")
-        sendGameStatus()
+        sendGameInformation()
         val indexOfPlayer = 0
         val indexOfFirstCardSelected = 0
         val indexOfSecondCardSelected = 2
@@ -138,7 +81,7 @@ class GameCoordinatorActorSpec extends ScalaTestWithActorTestKit
     "send information about the card drawn" when {
       "receive the command to draw a card from deck" in {
         skipFirstShowPhase()
-        sendGameStatus()
+        sendGameInformation()
         val message = gameCoordinatorProbe.expectMessageType[GameCoordinatorMessage.GameInformation]
         val (firstCardOfDeck, _) = message.game.deckStack.drawFirstCard
         drawCardFromDeck()
@@ -146,7 +89,7 @@ class GameCoordinatorActorSpec extends ScalaTestWithActorTestKit
       }
       "receive the command to draw a card from discard stack" in {
         skipFirstShowPhase()
-        sendGameStatus()
+        sendGameInformation()
         val message = gameCoordinatorProbe.expectMessageType[GameCoordinatorMessage.GameInformation]
         val (firstCardOfDiscardStack, _) = message.game.discardDeckStack.drawFirstCard
         drawCardFromDiscardStack()
@@ -158,14 +101,12 @@ class GameCoordinatorActorSpec extends ScalaTestWithActorTestKit
     "send new top card of discard card stack equal to card drawn" when {
       "receive the command to discard the card drawn without exchange any of own" in {
         skipFirstShowPhase()
-        sendGameStatus()
+        sendGameInformation()
         val game = gameCoordinatorProbe.expectMessageType[GameCoordinatorMessage.GameInformation].game
         println(game)
         val (cardDrawn, newDeck) = game.deckStack.drawFirstCard
-        drawCardFromDeck()
-        val _ = gameCoordinatorProbe.expectMessageType[CardDrawn]
-        discardCardDrawn()
-        val _ = gameCoordinatorProbe.expectMessageType[NewTopCardDiscardStack]
+        skipDrawCardFromDeckPhase()
+        skipDiscardCardDrawnPhase()
         endTurn()
         val newGameState = gameCoordinatorProbe.expectMessageType[GameCoordinatorMessage.GameInformation].game
         println(newGameState)
@@ -179,12 +120,11 @@ class GameCoordinatorActorSpec extends ScalaTestWithActorTestKit
         skipFirstShowPhase()
         val indexPlayer = 0
         val indexCardToDiscard = 2
-        sendGameStatus()
+        sendGameInformation()
         val game = gameCoordinatorProbe.expectMessageType[GameCoordinatorMessage.GameInformation].game
         println(game)
         val (cardDrawn, newDeck) = game.deckStack.drawFirstCard
-        drawCardFromDeck()
-        val _ = gameCoordinatorProbe.expectMessageType[CardDrawn]
+        skipDrawCardFromDeckPhase()
         val handCards = game.players(indexPlayer).hand.cards
         val cardToDiscard = handCards(indexCardToDiscard)
         discardNthCard(indexCardToDiscard)
@@ -193,32 +133,8 @@ class GameCoordinatorActorSpec extends ScalaTestWithActorTestKit
       }
     }
 
-
-    // during the game there are more ways to see the cards
-    "send card value" when {
-      // at start or power
-      "receive the command to see one of own card" in {
-        // TODO:
-        //  È possibile farlo all'inizio del gioco o quando hai una carta potere
-        //  effettuare questo test per la fase iniziale in cui puoi vedere due carte
-        val indexPlayer = 0
-        val indexCardToSee = 2
-        sendGameStatus()
-        val game = gameCoordinatorProbe.expectMessageType[GameCoordinatorMessage.GameInformation].game
-        println(game)
-        val handCards = game.players(indexPlayer).hand.cards
-        val cardToSee = handCards(indexCardToSee)
-        showYourNthCard(indexCardToSee)
-        val cardSeen = gameCoordinatorProbe.expectMessageType[GameCoordinatorMessage.CardSeen].card
-        cardSeen mustBe cardToSee
-      }
-      //      // with power
-      //      "receive the command to see one card of opponents" in {
-      ////        fail("Not implemented yet")
-      //      }
-    }
-
     // powers implementation
+
     "send own card value" when {
       "drawn card with power to show one of own card" in {
         skipFirstShowPhase()
@@ -252,10 +168,79 @@ class GameCoordinatorActorSpec extends ScalaTestWithActorTestKit
         val newOwnCardAfterReplace = game.players(playerIndex).hand.cards(adversaryCardToChangeIndex)
         val newAdversaryCardAfterReplace = game.players(0).hand.cards(ownCardToChangeIndex)
         replaceOwnNthCardWithAdversaryNthOne(ownCardToChangeIndex, playerIndex, adversaryCardToChangeIndex)
-        sendGameStatus()
+        sendGameInformation()
         val gameInformation = gameCoordinatorProbe.expectMessageType[GameCoordinatorMessage.GameInformation].game
         gameInformation.players(0).hand.cards(ownCardToChangeIndex) mustBe newOwnCardAfterReplace
         gameInformation.players(1).hand.cards(adversaryCardToChangeIndex) mustBe newAdversaryCardAfterReplace
       }
     }
   }
+
+  private def skipDrawCardFromDeckPhase(): Unit =
+    drawCardFromDeck()
+    discardMessage()
+
+  private def skipDrawCardFromDiscardStackPhase(): Unit =
+    drawCardFromDiscardStack()
+    discardMessage()
+
+  private def skipDiscardCardDrawnPhase(): Unit =
+    discardCardDrawn()
+    discardMessage()
+
+  private def skipFirstShowPhase(): Unit =
+    showYourNthCard(0)
+    showYourNthCard(0)
+    discardMessages(2)
+
+  private def skipPower(power: Power): Unit =
+    power match
+      case Power.SeeYourCard() => showYourNthCard(0); discardMessage()
+      case Power.SeeYourOpponentCard() => showAdversaryNthCard(0, 0); discardMessage()
+      case Power.ChangeOneOfYourCardWithOpponent() => replaceOwnNthCardWithAdversaryNthOne(0, 0, 0)
+
+  private def skipARound(): Unit =
+    drawCardFromDeck()
+    val power = gameCoordinatorProbe.expectMessageType[CardDrawn].card.power
+    if power != Power.NoPower() then skipPower(power)
+    discardCardDrawn()
+    discardMessage()
+    endTurn()
+    val gameInformation = gameCoordinatorProbe.expectMessageType[GameCoordinatorMessage.GameInformation].game
+    newTurn(gameInformation)
+
+  private def skipRoundsUntilDrawThisPower(power: Power): (Card, Game.GameInProgress) =
+    sendGameInformation()
+    var game = gameCoordinatorProbe.expectMessageType[GameCoordinatorMessage.GameInformation].game
+    while game.deckStack.drawFirstCard._1.power != power do
+      skipARound()
+      sendGameInformation()
+      game = gameCoordinatorProbe.expectMessageType[GameCoordinatorMessage.GameInformation].game
+    drawCardFromDeck()
+    val cardDrawn = gameCoordinatorProbe.expectMessageType[CardDrawn].card
+    (cardDrawn, game)
+
+  private def sendGameInformation(): Unit = gameCoordinatorActor ! GameCoordinatorMessage.SendGameStatus(gameCoordinatorProbe.ref)
+
+  private def drawCardFromDeck(): Unit = gameCoordinatorActor ! GameCoordinatorMessage.DrawCardFromDeck()
+
+  private def drawCardFromDiscardStack(): Unit = gameCoordinatorActor ! GameCoordinatorMessage.DrawCardFromDiscardStack()
+
+  private def discardCardDrawn(): Unit = gameCoordinatorActor ! GameCoordinatorMessage.DiscardCardDrawn()
+
+  private def discardNthCard(index: Int): Unit = gameCoordinatorActor ! GameCoordinatorMessage.DiscardYourNthCard(index)
+
+  private def showYourNthCard(i: Int): Unit = gameCoordinatorActor ! GameCoordinatorMessage.ShowYourNthCard(i)
+
+  private def showAdversaryNthCard(playerIndex: Int, cardIndex: Int): Unit = gameCoordinatorActor ! GameCoordinatorMessage.ShowAdversaryNthCard(playerIndex, cardIndex)
+
+  private def replaceOwnNthCardWithAdversaryNthOne(ownCardIndex: Int, adversaryIndex: Int, adversaryCardIndex: Int): Unit =
+    gameCoordinatorActor ! GameCoordinatorMessage.ReplaceOwnNthCardWithAdversaryNthOne(ownCardIndex, adversaryIndex, adversaryCardIndex)
+
+  private def endTurn(): Unit = gameCoordinatorActor ! GameCoordinatorMessage.EndTurn()
+
+  private def newTurn(game: Game.GameInProgress): Unit = gameCoordinatorActor ! GameCoordinatorMessage.NewTurn(game)
+
+  private def discardMessage(): Unit = discardMessages(1)
+
+  private def discardMessages(n: Int): Unit = gameCoordinatorProbe.receiveMessages(n)
