7489d3d36ae9ee25760a3b78002611a8cbe94333
Michele Bachetti <michele.bachetti@studio.unibo.it>
2025-05-22 18:43:02 +0200

    feat: add test for client and server working together

---
+++ src/test/scala/SystemTest.scala
@@ -0,0 +1,62 @@
+import akka.actor.testkit.typed.scaladsl.{ScalaTestWithActorTestKit, TestProbe}
+import akka.actor.typed.ActorRef
+import akka.actor.typed.receptionist.Receptionist
+import akka.actor.typed.scaladsl.Behaviors
+import akka.cluster.typed.{Cluster, Join}
+import controller.Client
+import model.Game.GameInConstruction
+import model.{GameParameters, PlayerInLobby}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
+import utils.Message
+import utils.ServerMessages.{RegisterGame, ServerKey}
+import utils.ViewMessages.CreateNewGame
+
+import scala.concurrent.duration.DurationInt
+
+class SystemTest extends ScalaTestWithActorTestKit
+  with AnyWordSpecLike
+  with BeforeAndAfterAll
+  with BeforeAndAfterEach
+  with Matchers:
+
+  var probeServer: TestProbe[Message] = _
+  var server: ActorRef[Message] = _
+
+  override def beforeAll(): Unit =
+    val cluster = Cluster.get(testKit.system)
+    cluster.manager.tell(Join.create(cluster.selfMember.address))
+
+  override def beforeEach(): Unit = {
+    probeServer = testKit.createTestProbe[Message]()
+    server = testKit.spawn(Behaviors.monitor(probeServer.ref, Server()))
+
+    val probe = TestProbe[Receptionist.Listing]()
+    eventually(timeout(3.seconds), interval(100.millis)) {
+      system.receptionist ! Receptionist.Find(ServerKey, probe.ref)
+      val listing = probe.receiveMessage()
+      assert(listing.serviceInstances(ServerKey).contains(server))
+    }
+  }
+
+  override def afterAll(): Unit = testKit.shutdownTestKit()
+
+  "Client" should {
+    "create game correctly and send it to the server" in {
+      val defaultName = "defaultCoolName"
+
+      val hostUserID = "Player01"
+      val probeClientHost = testKit.createTestProbe[Message]()
+      val clientHost = testKit.spawn(Behaviors.monitor(probeClientHost.ref, Client(hostUserID)))
+
+      //      val probeClientJoiner = testKit.createTestProbe[Message]()
+      //      val clientJoiner = testKit.spawn(Behaviors.monitor(probeClientJoiner.ref, Client("Player02")))
+
+      clientHost ! CreateNewGame(makePublic = true, maxTimeRound = 10, maxNumRound = 5, maxPlayers = 4)
+
+      probeServer.expectMessage(10.seconds, RegisterGame(
+        GameInConstruction(hostUserID + "game", GameParameters(true, 10, 5, 4), List(PlayerInLobby(hostUserID, defaultName, clientHost))),
+        clientHost))
+    }
+  }
