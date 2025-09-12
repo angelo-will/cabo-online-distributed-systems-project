9f834c1ce6dd1c2d99f878a36a9bf488361ebf02
Angelo Tinti <angelo.tinti@studio.unibo.it>
2025-09-12 10:31:09 +0200

    Add message DuringGameViewReady to notify to Client the actor is created.

--- cabo-client/src/main/scala/utils/ClientMessages.scala
+++ cabo-client/src/main/scala/utils/ClientMessages.scala
@@ -75,5 +75,7 @@ object ClientMessages {
   case class TakeGetInProgressGame(game: GameInProgress) extends ClientCommand
   
   case class TurnEnded(game: GameInProgress, turnLog: TurnLog) extends ClientCommand
+  
+  case class DuringGameViewReady(viewRef: ActorRef[Message]) extends ClientCommand
 
 }
