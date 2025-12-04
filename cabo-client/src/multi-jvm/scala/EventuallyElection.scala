2535978c3a0aa1a56662e8a74915137eb71884a8
Michele Bachetti <michele.bachetti@studio.unibo.it>
2025-12-04 17:30:50 +0100

    feat: add generic test for new host election

---
+++ cabo-client/src/multi-jvm/scala/EventuallyElection.scala
@@ -0,0 +1,180 @@
+import akka.actor.testkit.typed.scaladsl.TestProbe
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.receptionist.{Receptionist, ServiceKey}
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.adapter.*
+import akka.cluster.Cluster
+import akka.cluster.ClusterEvent.{CurrentClusterState, MemberUp}
+import akka.remote.testkit.MultiNodeSpec
+import akka.testkit.ImplicitSender
+import controller.Client
+import controller.Client.*
+import messages.ClientMessages
+import org.scalatest.concurrent.Eventually.eventually
+import org.scalatest.concurrent.Futures.{interval, timeout}
+import messages.ClientMessages.{JoinAddress, StartTheGame, TakeGetInProgressGame}
+import utils.Message
+
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
+import scala.language.implicitConversions
+
+class EventuallyElectionMultiJvmNode1 extends EventuallyElection
+class EventuallyElectionMultiJvmNode2 extends EventuallyElection
+class EventuallyElectionMultiJvmNode3 extends EventuallyElection
+
+abstract class EventuallyElection extends MultiNodeSpec(MultiNodeConfig) with STMultiNodeSpec with ImplicitSender {
+
+  import MultiNodeConfig.*
+
+  override def initialParticipants: Int = roles.size
+
+  implicit val typedSystem: ActorSystem[Nothing] = system.toTyped
+
+  "Client on different nodes" should {
+
+    "illustrate how to startup cluster" in within(15.seconds) {
+      Cluster(system).subscribe(testActor, classOf[MemberUp])
+      expectMsgClass(classOf[CurrentClusterState])
+
+      val firstAddress = node(node1).address
+      val secondAddress = node(node2).address
+      val thirdAddress = node(node3).address
+      Cluster(system).join(firstAddress)
+
+      receiveN(3).collect { case MemberUp(m) => m.address }.toSet should be(
+        Set(firstAddress, secondAddress, thirdAddress))
+
+      Cluster(system).unsubscribe(testActor)
+
+      testConductor.enter("all-up")
+    }
+
+    "change host if the host disconnects with multiple elections" in {
+      runOn(node1) {
+        // Client code
+        val probeClient = TestProbe[Message]()
+        val viewProbe = TestProbe[Message]()
+        val client = system.spawn(Behaviors.monitor(probeClient.ref, Client("client5-1/", "Gino", viewProbe.ref)), "client5-1")
+
+        enterBarrier("game-created")
+
+        val probe = TestProbe[Receptionist.Listing]()
+        eventually(timeout(3.seconds), interval(100.millis)) {
+          typedSystem.receptionist ! Receptionist.Find(ServiceKey[Message]("host5game"), probe.ref)
+          val listing = probe.receiveMessage()
+          assert(listing.serviceInstances(ServiceKey[Message]("host5game")).map(_.path.name).contains("Host4"))
+        }
+
+        client ! ClientMessages.JoinAGame()
+        probeClient.expectMessage(ClientMessages.JoinAGame())
+
+        client ! JoinAddress("host5game")
+        probeClient.expectMessage(ClientMessages.JoinAddress("host5game"))
+
+        probeClient.expectMessageType[YouJoinedTheGame]
+
+        enterBarrier("player2-joined")
+
+        enterBarrier("player3-joined")
+
+        probeClient.expectMessageType[UpdateAboutGame]
+
+        enterBarrier("game-started")
+
+        probeClient.expectMessageType[GameHasStarted]
+
+        val exitFuture = testConductor.exit(node2, 0)
+        Await.result(exitFuture, 30.seconds)
+
+        enterBarrier("host5-removed")
+
+        eventually(timeout(10.seconds), interval(500.millis)) {
+          val m = probeClient.receiveMessage()
+          assert(m.isInstanceOf[ElectionWon])
+        }
+
+      }
+
+      runOn(node2) {
+        // Host code
+        val probeHost = TestProbe[Message]()
+        val viewProbe = TestProbe[Message]()
+        val host = system.spawn(Behaviors.monitor(probeHost.ref, Client("host5", "Gino", viewProbe.ref)), "Host4")
+
+        host ! ClientMessages.CreateNewGame(gameCode = Some("host5game"))
+        probeHost.receiveMessages(1)
+
+        val probe = TestProbe[Receptionist.Listing]()
+        eventually(timeout(3.seconds), interval(100.millis)) {
+          typedSystem.receptionist ! Receptionist.Find(ServiceKey[Message]("host5game"), probe.ref)
+          val listing = probe.receiveMessage()
+          assert(listing.serviceInstances(ServiceKey[Message]("host5game")).contains(host))
+        }
+
+        enterBarrier("game-created")
+
+        probeHost.expectMessageType[IWantToPlay]
+
+        enterBarrier("player2-joined")
+
+        probeHost.expectMessageType[IWantToPlay]
+
+        enterBarrier("player3-joined")
+
+        host ! StartTheGame()
+        probeHost.expectMessageType[StartTheGame]
+
+        probeHost.expectMessageType[StartGameBehavior]
+
+        probeHost.expectMessageType[TakeGetInProgressGame]
+
+        enterBarrier("game-started")
+
+      }
+
+      runOn(node3) {
+        // Another Client code
+        val probeClient = TestProbe[Message]()
+        val viewProbe = TestProbe[Message]()
+        val client = system.spawn(Behaviors.monitor(probeClient.ref, Client("client5-2/", "Gino", viewProbe.ref)), "client5-2")
+
+        enterBarrier("game-created")
+
+        val probe = TestProbe[Receptionist.Listing]()
+        eventually(timeout(3.seconds), interval(100.millis)) {
+          typedSystem.receptionist ! Receptionist.Find(ServiceKey[Message]("host5game"), probe.ref)
+          val listing = probe.receiveMessage()
+          assert(listing.serviceInstances(ServiceKey[Message]("host5game")).map(_.path.name).contains("Host4"))
+        }
+
+        enterBarrier("player2-joined")
+
+        client ! ClientMessages.JoinAGame()
+        probeClient.expectMessage(ClientMessages.JoinAGame())
+
+        client ! JoinAddress("host5game")
+        probeClient.expectMessage(ClientMessages.JoinAddress("host5game"))
+
+        probeClient.expectMessageType[YouJoinedTheGame]
+
+        enterBarrier("player3-joined")
+
+        enterBarrier("game-started")
+
+        probeClient.expectMessageType[GameHasStarted]
+
+        enterBarrier("host5-removed")
+
+        eventually(timeout(10.seconds), interval(500.millis)) {
+          val m = probeClient.receiveMessage()
+          assert(m.isInstanceOf[NewHostElected])
+          assert(m.asInstanceOf[NewHostElected].replyTo.toString.toLowerCase contains "client5-1")
+        }
+
+      }
+    }
+    enterBarrier("test-completed")
+  }
+
+}
--- cabo-client/src/multi-jvm/scala/MultiNodeUtils.scala
+++ cabo-client/src/multi-jvm/scala/MultiNodeUtils.scala
@@ -10,9 +10,9 @@ import scala.language.implicitConversions
 trait STMultiNodeSpec extends MultiNodeSpecCallbacks with AnyWordSpecLike with Matchers with BeforeAndAfterAll with BeforeAndAfterEach {
   self: MultiNodeSpec =>
 
-  override def beforeAll() = multiNodeSpecBeforeAll()
+  override def beforeAll(): Unit = multiNodeSpecBeforeAll()
 
-  override def afterAll() = multiNodeSpecAfterAll()
+  override def afterAll(): Unit = multiNodeSpecAfterAll()
 
   // Might not be needed anymore if we find a nice way to tag all logging from a node
   override implicit def convertToWordSpecStringWrapper(s: String): WordSpecStringWrapper =
