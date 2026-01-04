f610952a5ce36efda00a09b621a01c5778e733f8
Angelo Tinti <angelo.tinti@studio.unibo.it>
2026-01-04 18:11:59 +0100

    Refactor CaboAkkaUtils

--- cabo-commons/src/main/scala/akka/CaboAkkaUtils.scala
+++ cabo-commons/src/main/scala/akka/CaboAkkaUtils.scala
@@ -9,17 +9,19 @@ object CaboAkkaUtils {
 
   val seeds: List[Int] = List(2551, 2552) // seed used in the configuration
 
-  def startup[X](port: Int = 0, configuration: Config = ConfigFactory.load("application.conf"))(root: => Behavior[X]): ActorSystem[X] =
-    // Override the configuration of the port
-    // Siccome sono nella stessa macchina devo usare porte diverse per simulare nodi diversi
+  def startup[X](port: Int = 0, configuration: Config = ConfigFactory.load("application.conf"))(root: => Behavior[X]): ActorSystem[X] = {
     val config = ConfigFactory
       .parseString(s"""akka.remote.artery.canonical.port=$port""")
       .withFallback(configuration)
 
-    // Create an Akka system
     ActorSystem(root, "ClusterSystem", config)
+  }
 
-  def startupWithRole[X](role: String, port: Int = 0, configuration: Config = ConfigFactory.load("application.conf"))(root: => Behavior[X]): ActorSystem[X] =
+  def startupWithRole[X](
+                          role: String,
+                          port: Int = 0,
+                          configuration: Config = ConfigFactory.load("application.conf")
+                        )(root: => Behavior[X]): ActorSystem[X] = {
     val config = ConfigFactory
       .parseString(
         s"""
@@ -27,11 +29,6 @@ object CaboAkkaUtils {
         """)
       .withFallback(configuration)
 
-    // Create an Akka system
     startup(port, config)(root)
-
-  def deployActor(behavior: Behavior[Message])(actorName: String): Behavior[Message] = Behaviors.setup { ctx =>
-    ctx.spawn(behavior, actorName)
-    Behaviors.empty
   }
 }
