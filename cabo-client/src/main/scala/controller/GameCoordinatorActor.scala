4981a7f212b45414a2b2bcf290e4ad77440e88f7
Angelo Tinti <angelo.tinti@studio.unibo.it>
2026-01-23 10:54:20 +0100

    Game coordinator update own version of game when empty turn is requested

--- cabo-client/src/main/scala/controller/GameCoordinatorActor.scala
+++ cabo-client/src/main/scala/controller/GameCoordinatorActor.scala
@@ -272,7 +272,11 @@ object GameCoordinatorActor:
       val log = new PlayCycleTurnLog(UserBase(userID, nameOfPlayer), actualTurn)
       log.addEvent(TurnEvent.JumpTurnForDisconnection())
       gameData.clientReference ! CLMsg.TurnEnded(gameTurnUpdated, log)
-      Behaviors.same
+      val x = gameData.copy(
+        game = gameData.game.copy(currentRound = actualTurn + 1),
+        temporaryGame = gameData.game.copy(currentRound = actualTurn + 1)
+      )
+      notMyTurn(x)
   }
 
   private def handleWhoIsPlaying(gameData: GameData): PartialFunction[(ActorContext[IGameCoordinatorMessage], IGameCoordinatorMessage), Behavior[IGameCoordinatorMessage]] = {
